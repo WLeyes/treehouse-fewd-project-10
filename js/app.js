ed15a36ecd47f8a14da9d1a7059fb47023ef01de
Warren Leyes <warren.leyes@gmail.com>
2018-05-23 12:17:46 -0700

    adding 2 sided cardf with 3d transform

--- js/app.js
+++ js/app.js
@@ -3,9 +3,9 @@ const DataCtrl = ( () => {
   
   // Data Fetch API
   function fetchUserJSON(url,numberOfUsers){  
-    fetch(`${url}?seed=b73e4d2e35f01cdc&inc=picture,name,email,location&nat=us&results=${parseInt(numberOfUsers)}`)
+    fetch(`${url}?seed=b73e4d2e35f01cdc&nat=us&results=${parseInt(numberOfUsers)}`)
       .then(response => response.json())
-      .then(data => UICtrl.employees(data))
+      .then(data => UICtrl.card(data))
       .catch(error => console.log(error));
   }
 
@@ -28,15 +28,27 @@ const UICtrl = ( () => {
   }
 
   return {
-    employees: (data) => {
+    card: (data) => {
       console.log(data);
 
       for(let i = 0; i < data.results.length; i++){
         let image      = data.results[i].picture.medium;
+        
         let firstName  = data.results[i].name.first;
         let lastName   = data.results[i].name.last;
+        let birthday   = data.results[i].dob;
         let email      = data.results[i].email;
+        let cell       = data.results[i].cell;
+
+        let street     = data.results[i].location.street;
         let city       = data.results[i].location.city;
+        let state      = data.results[i].location.state;
+        let postal     = data.results[i].location.postcode;
+        
+        let next;
+        let previous;
+        let close;
+        let currentEmployee;
         
         let employee   = `
         <div class="card">
@@ -44,6 +56,15 @@ const UICtrl = ( () => {
           <p>${firstName} ${lastName}</p>
           <a href="mailto:${email}" class="link">${email}</a>
           <p>${city}</p>
+          <div class="card--details">
+            <img src="${image}" alt="${firstName} ${lastName}'s profile picture" class="img--profile"></img>
+            <p>${firstName} ${lastName}</p>
+            <a href="mailto:${email}" class="link">${email}</a>
+            <p>${city}</p>
+            <p>${cell}</p>
+            <p>${street} ${city}, ${state} ${postal}</p>
+            <p>Birthday: ${birthday}</p>
+          </div>
         </div>
         `;
 
